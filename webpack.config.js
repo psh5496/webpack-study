35aa08cdce65231be9d88010393949b040f93674
psh549633 <psh549633@gmail.com>
2022-07-26 11:31:27 +0900

    :wrench: :: webpack 설정

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.[hash].js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+    port: port,
+    open: true,
+    historyApiFallback: true,
+  },
+};
